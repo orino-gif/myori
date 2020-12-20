1f4ec78ed0c5e358964d7143e6cf9981385798cf
norio <orino0710@gmail.com>
2020-12-20 15:45:53 +0900

    本番環境の為の変更。主にアセットパイプライン周り

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,10 +28,10 @@ Rails.application.configure do
   #config.assets.js_compressor = nil
 
   #config.assets.css_compressor = nil
-  config.assets.css_compressor = :sass
+  config.assets.css_compressor = :scss
   
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
---
+++ ruby/require_file.rb
@@ -0,0 +1,2 @@
+require_relative 'puts_hello'
+

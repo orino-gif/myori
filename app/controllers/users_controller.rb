df7cb805d19071034ed750ad1cee4fb071de4d5b
norio <orino0710@gmail.com>
2021-01-15 15:25:58 +0900

    pryをインストール

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def create
      User.create(name:params[:name],email:params[:email],
      passwd:params[:pass],)
+     flash[:notice] = "ユーザー登録が完了しました"
   end
   def manage
      @users=User.all
